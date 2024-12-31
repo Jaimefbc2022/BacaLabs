e52b74f777225741bfa01b5ad6936c48dfc88e05
Jaimefbc2022 <jaimefbaca@gmail.com>
2024-12-31 10:34:32 +0100

    Update 1_👋_Hello.py

--- 1_👋_Hello.py
+++ 1_👋_Hello.py
@@ -20,13 +20,25 @@ st.sidebar.success("Select a demo above.")
 st.markdown(
     """
     Here, you'll find a collection of simple yet useful apps designed to make life a little easier.
-    My goal is to share tools that I’ve created and believe can help others—whether it’s solving a problem,
-    saving time, or just simplifying a task.
+    """
+)
 
-    To kick things off, I’ve built a Tax Calculator tailored for Spain and the UK, helping you quickly estimate
-    taxes with ease. But this is just the beginning! I’ll be adding more handy apps over time, so stay tuned for updates.
+st.markdown(
+    """
+    My goal is to share tools that I’ve created and believe can help others—whether it’s solving a problem, saving time, or just simplifying a task.
+    
+    """
+)
 
+st.markdown(
+    """
+    To kick things off, I’ve built a Tax Calculator tailored for Spain and the UK, helping you quickly estimate taxes with ease. But this is just the beginning! I’ll be adding more handy apps over time, so stay tuned for updates.
+    """
+)
+st.markdown(
+    """
     Feel free to explore, try them out, and share your feedback. Let’s make this a place where practical ideas come to life! 😊
-"""
+    
+    """
 )
 
